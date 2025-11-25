9639c00e2db0bdaefc321bd73aabe430dff243bf
Mohammed Othman <m.othman1198@gmail.com>
2025-11-25 22:08:04 +0300

    fetch input cities data + cities list UI

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,11 @@ import { CountryWeather } from "./components/CountryWeather";
 import { WeatherDataCard } from "./components/WeatherDataCard";
 import { DailyForecastCard } from "./components/DailyForecastCard";
 import { HourlyForecastAside } from "./components/HourlyForecastAside";
-import { SearchForm, type GetLocationWeather } from "./components/SearchForm";
-import { useState } from "react";
+import { SearchForm } from "./components/SearchForm";
 
 function App() {
-  const [weatherData, setWeatherData] = useState<GetLocationWeather>({
-    cityName: "",
-    current: {},
-    daily: {},
-  });
-  const [isError, setIsError] = useState(false);
   return (
-    <div className="w-full h-dvh bg-(--bg-color) font-dm-sans">
+    <div className={`w-full bg-(--bg-color) font-dm-sans`}>
       <div className="lg:max-w-(--desktop-width) w-[90%] mx-auto pt-8 max-[667px]:pt-3 max-[667px]:w-[95%]">
         <header className="flex justify-between">
           <Logo />
@@ -27,24 +20,18 @@ function App() {
           <h1 className="font-bricolage text-[50px] font-bold text-(--main-heading-color) max-[376px]:w-72 max-[376px]:mx-auto  max-[668px]:text-center">
             How's the sky looking today?
           </h1>
-          <SearchForm setWeatherData={setWeatherData} setIsError={setIsError} />
+          <SearchForm />
         </div>
-        {isError ? (
-          <p className="text-center text-white font-bold mt-12 text-2xl">
-            No Search result found!
-          </p>
-        ) : (
-          <div className="flex max-[668px]:flex-col mt-8 gap-5 pb-16">
-            <div className="flex-3 max-[376px]:mx-auto">
-              <CountryWeather />
-              <WeatherDataCard />
-              <DailyForecastCard />
-            </div>
-            <div className="flex-1">
-              <HourlyForecastAside />
-            </div>
+        <div className="flex max-[668px]:flex-col mt-8 gap-5 pb-16">
+          <div className="flex-3 max-[376px]:mx-auto">
+            <CountryWeather />
+            <WeatherDataCard />
+            <DailyForecastCard />
           </div>
-        )}
+          <div className="flex-1">
+            <HourlyForecastAside />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,11 +1,37 @@
-import { type Ref } from "react";
+import { useState, type ChangeEvent } from "react";
 import SearchIcon from "../assets/icon-search.svg";
 
-type Props = {
-  inputRef: Ref<HTMLInputElement>;
-};
+export const SearchInput = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [citiesList, setCitiesList] = useState([]);
 
-export const SearchInput = ({ inputRef }: Props) => {
+  const handleCitiesList = async (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+
+    if (value.trim().length === 0) {
+      setCitiesList([]);
+      return;
+    }
+
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${value}`
+    );
+    const data = await response.json();
+
+    if (!data.results) {
+      setCitiesList([]);
+      return;
+    }
+
+    setCitiesList(data.results);
+  };
+
+  const handleOnClick = (city) => {
+    setInputValue(`${city.name}, ${city.country}`);
+    setCitiesList([]);
+  };
+
   return (
     <div className="relative">
       <img
@@ -13,13 +39,31 @@ export const SearchInput = ({ inputRef }: Props) => {
         src={SearchIcon}
         alt=""
       />
+
       <input
-        className="w-[500px] h-10 px-12 rounded-lg bg-(--search-input-bg-color) focus:outline focus:outline-2 focus:outline-white focus:outline-offset-4 border-black text-(--search-input-text-color) max-[667px]:w-full hover:bg-(--search-input-hover-bg-color) transition duration-300"
+        className="w-[500px] h-10 pl-12 rounded-lg bg-(--search-input-bg-color) focus:outline focus:outline-2 focus:outline-white focus:outline-offset-4 border-black text-(--search-input-text-color) max-[667px]:w-full hover:bg-(--search-input-hover-bg-color) transition duration-300"
         placeholder="Search for a place..."
         type="text"
-        ref={inputRef}
         name="search-input"
+        value={inputValue}
+        onChange={handleCitiesList}
       />
+
+      {citiesList.length > 0 && (
+        <div className="absolute w-[500px] max-h-52 overflow-y-auto left-0 top-13 px-2.5 py-2.5 rounded-lg bg-(--search-input-bg-color) text-white z-10">
+          <ul className="flex flex-col gap-2.5 text-left">
+            {citiesList.map((city) => (
+              <li
+                key={city.id}
+                onClick={() => handleOnClick(city)}
+                className="hover:bg-(--days-dropdown-menu-bg-color) pl-2.5 py-2 rounded-lg cursor-pointer"
+              >
+                {city.name}, {city.country}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
